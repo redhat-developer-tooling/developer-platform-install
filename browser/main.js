d9e72cf87e8939398248ee55bce919eaecfb0ad9
Ken Finnigan <ken@kenfinnigan.me>
2015-09-25 09:59:35 -0400

    adjust alignment to make it more readable

--- browser/main.js
+++ browser/main.js
@@ -6,35 +6,36 @@ import confCtrl from './confirm/controller';
 import instCtrl from './install/controller';
 import startCtrl from './start/controller';
 
-let mainModule = angular.module('devPlatInstaller', ['ui.router'])
-                        .controller(acctCtrl.name, acctCtrl)
-                        .controller(confCtrl.name, confCtrl)
-                        .controller(instCtrl.name, instCtrl)
-                        .controller(startCtrl.name, startCtrl)
-                        .config( ["$stateProvider", "$urlRouterProvider", ($stateProvider, $urlRouterProvider) => {
-                          $urlRouterProvider.otherwise('/account');
+let mainModule =
+      angular.module('devPlatInstaller', ['ui.router'])
+          .controller(acctCtrl.name, acctCtrl)
+          .controller(confCtrl.name, confCtrl)
+          .controller(instCtrl.name, instCtrl)
+          .controller(startCtrl.name, startCtrl)
+          .config( ["$stateProvider", "$urlRouterProvider", ($stateProvider, $urlRouterProvider) => {
+            $urlRouterProvider.otherwise('/account');
 
-                          $stateProvider
-                            .state('account', {
-                              url: '/account',
-                              controller: 'AccountController as acctCtrl',
-                              templateUrl: 'account/account.html'
-                            })
-                            .state('confirm', {
-                              url: '/confirm',
-                              controller: 'ConfirmController as confCtrl',
-                              templateUrl: 'confirm/confirm.html'
-                            })
-                            .state('install', {
-                              url: '/install',
-                              controller: 'InstallController as instCtrl',
-                              templateUrl: 'install/install.html'
-                            })
-                            .state('start', {
-                              url: '/start',
-                              controller: 'StartController as startCtrl',
-                              templateUrl: 'start/start.html'
-                            });
-                        }]);
+            $stateProvider
+              .state('account', {
+                url: '/account',
+                controller: 'AccountController as acctCtrl',
+                templateUrl: 'account/account.html'
+              })
+              .state('confirm', {
+                url: '/confirm',
+                controller: 'ConfirmController as confCtrl',
+                templateUrl: 'confirm/confirm.html'
+              })
+              .state('install', {
+                url: '/install',
+                controller: 'InstallController as instCtrl',
+                templateUrl: 'install/install.html'
+              })
+              .state('start', {
+                url: '/start',
+                controller: 'StartController as startCtrl',
+                templateUrl: 'start/start.html'
+              });
+          }]);
 
 export default mainModule;
