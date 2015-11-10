07e322dfa61cfd146944b7e2db1336d790cfcd60
Ken Finnigan <ken@kenfinnigan.me>
2015-11-10 00:26:21 -0500

    Add SAML Auth

--- src/browser/account/controller.js
+++ src/browser/account/controller.js
@@ -1,13 +1,36 @@
 class AccountController {
-  constructor($state) {
+  constructor($state, $http) {
     this.router = $state;
+    this.http = $http;
+
+    this.username = "";
+    this.password = "";
   }
 
   login() {
-    this.router.go('confirm');
+    var req = {
+      method: 'GET',
+      url: 'https://idp.redhat.com/idp/authUser?' +
+        'j_username=' + this.username +
+        '&j_password=' + this.password +
+        '&redirect=https://access.redhat.com/jbossnetwork/restricted/listSoftware.html'
+    };
+
+    this.http(req)
+      .then(result => {
+        if (result.status == 200) {
+          console.log(result.data)
+          this.router.go('confirm');
+        } else {
+          console.log('Failed to authenticate')
+        }
+      },
+      failure => {
+        console.log('Failed to authenticate')
+      });
   }
 }
 
-AccountController.$inject = ['$state'];
+AccountController.$inject = ['$state', '$http'];
 
 export default AccountController;
