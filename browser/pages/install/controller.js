7fc97c37b53ea260018f48b35211f10a8d5c6119
Ken Finnigan <ken@kenfinnigan.me>
2015-11-23 14:27:56 -0500

    Push download and install functions for each piece into a queue so they can be executed in async

--- browser/pages/install/controller.js
+++ browser/pages/install/controller.js
@@ -18,23 +18,28 @@ class InstallController {
 
       if (value.isDownloadRequired() && !value.isDownloaded()) {
         this.installerDataSvc.startDownload(key);
-        value.downloadInstaller(itemProgress,
-          () => {
-            this.installerDataSvc.downloadDone(itemProgress, key);
-          },
-          (error) => {
-            alert(error);
-          }
+
+        $timeout(
+          value.downloadInstaller(itemProgress,
+            () => {
+              $timeout(this.installerDataSvc.downloadDone(itemProgress, key));
+            },
+            (error) => {
+              alert(error);
+            }
+          )
         );
       } else if (!value.hasExistingInstall()) {
         this.installerDataSvc.startInstall(key);
-        value.install(itemProgress,
-          () => {
-            this.installerDataSvc.installDone(key);
-          },
-          (error) => {
-            alert(error);
-          }
+        $timeout(
+          value.install(itemProgress,
+            () => {
+              this.installerDataSvc.installDone(key);
+            },
+            (error) => {
+              alert(error);
+            }
+          )
         );
       }
     });
@@ -54,9 +59,8 @@ class InstallController {
 }
 
 class ProgressState {
-  constructor($scope, $timeout) {
+  constructor($scope) {
     this.$scope = $scope;
-    this.$timeout =
     this.current = 0;
     this.label = '';
     this.desc = '';
