09e92cedfc4cd084a8a1e364998a6fe4f0daa325
Ken Finnigan <ken@kenfinnigan.me>
2015-12-03 16:04:39 -0500

    Update to latest JBDS snapshot

--- browser/main.js
+++ browser/main.js
@@ -101,7 +101,7 @@ let mainModule =
             installerDataSvc.addItemToInstall(
                 'jbds',
                 new JbdsInstall(installerDataSvc,
-                                'https://devstudio.redhat.com/9.0/snapshots/builds/devstudio.product_9.0.mars/latest/all/jboss-devstudio-9.1.0.Beta1-v20151122-1948-B143-installer-standalone.jar',
+                                'https://devstudio.redhat.com/9.0/snapshots/builds/devstudio.product_9.0.mars/latest/all/jboss-devstudio-9.1.0.Beta1-v20151203-0439-B156-installer-standalone.jar',
                                 null)
             );
 
