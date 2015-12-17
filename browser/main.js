e1c6c938957f402eb14d1fc66eeeaf84fba0cf6e
Ken Finnigan <ken@kenfinnigan.me>
2015-12-17 09:40:08 -0500

    Change OC Binary download to use specific build instead of just latest

--- browser/main.js
+++ browser/main.js
@@ -71,7 +71,7 @@ let mainModule =
                                 $timeout,
                                 'https://developers.redhat.com/download-manager/jdf/file/cdk-2.0.0-beta3.zip?workflow=direct',
                                 'http://cdk-builds.usersys.redhat.com/builds/11-Dec-2015/rhel-7.2-server-kubernetes-vagrant-scratch-7.2-1.x86_64.vagrant-virtualbox.box',
-                                'https://ci.openshift.redhat.com/jenkins/job/devenv_ami/lastSuccessfulBuild/artifact/origin/artifacts/release/openshift-origin-client-tools-v1.1-477-g0d4de0b-0d4de0b-windows.zip',
+                                'https://ci.openshift.redhat.com/jenkins/job/devenv_ami/2970/artifact/origin/artifacts/release/openshift-origin-client-tools-v1.1-487-g945a0c9-945a0c9-windows.zip',
                                 'https://github.com/redhat-developer-tooling/openshift-vagrant/archive/master.zip',
                                 'http://the.earth.li/~sgtatham/putty/latest/x86/pscp.exe',
                                 null)
