b1d90d833f7fdc1c25a53faabf43ac86037ed4e6
Alexey Kazakov <alkazako@redhat.com>
2016-01-14 15:23:06 -0500

    DEVEX-82 Option to start Developer Studio on installer exit

--- browser/pages/start/controller.js
+++ browser/pages/start/controller.js
@@ -1,8 +1,102 @@
 'use strict';
 
+let remote = require('remote');
+let path = require('path');
+let fs = require('fs-extra');
+
+import Logger from '../../services/logger';
+
 class StartController {
-  constructor() {
+  
+  constructor(installerDataSvc) {
+    this.startJBDS = true;
+    this.installerDataSvc = installerDataSvc;
+  }
+
+  start() {
+    if(this.startJBDS) {
+      // Start JBDS
+      this.launchJBDS();
+    } else {
+      this.exit();
+    }
+  }
+
+  launchJBDS() {
+    Logger.info('JBDS Start - Write temp files...');
+    let jbdevstudioBat = path.join(this.installerDataSvc.jbdsDir(), 'jbdevstudio.bat');
+
+    let resetvarsBatFile = path.join(this.installerDataSvc.tempDir(), 'resetvars.bat');
+    let resetvarsVbsFile = path.join(this.installerDataSvc.tempDir(), 'resetvars.vbs');
+    let resetvarsVbsFileData = [
+        'Set oShell = WScript.CreateObject("WScript.Shell")',
+        'filename = oShell.ExpandEnvironmentStrings("' + resetvarsBatFile + '")',
+        'Set objFileSystem = CreateObject("Scripting.fileSystemObject")',
+        'Set oFile = objFileSystem.CreateTextFile(filename, TRUE)',
+
+        'set oEnv=oShell.Environment("System")',
+        'for each sitem in oEnv',
+        'oFile.WriteLine("SET " & sitem)',
+        'next',
+        'path = oEnv("PATH")',
+
+        'set oEnv=oShell.Environment("User")',
+        'for each sitem in oEnv',
+        'oFile.WriteLine("SET " & sitem)',
+        'next',
+
+        'path = path & ";" & oEnv("PATH")',
+        'oFile.WriteLine("SET PATH=" & path)',
+        'oFile.Close'
+    ].join('\r\n');
+    Logger.info('JBDS Start - Write ' + resetvarsVbsFile);
+    fs.writeFileSync(resetvarsVbsFile, resetvarsVbsFileData);
+    Logger.info('JBDS Start - Write ' + resetvarsVbsFile + ' SUCCESS');
+
+    let runJbdsFile = path.join(this.installerDataSvc.tempDir(), 'runjbds.bat');
+    let runJbdsFileData = [
+        '"' + resetvarsVbsFile + '"',
+        'call "' + resetvarsBatFile + '"',
+        'call "' + jbdevstudioBat + '"'
+    ].join('\r\n');
+    Logger.info('JBDS Start - Write ' + runJbdsFile);
+    fs.writeFileSync(runJbdsFile, runJbdsFileData);
+    Logger.info('JBDS Start - Write ' + runJbdsFile + ' SUCCESS');
+
+    Logger.info('JBDS Start - Write temp file SUCCESS');
+
+    Logger.info('JBDS Start - Run ' + runJbdsFile);
+    let runJbdsBat = require('child_process')
+        .spawn(
+            'cmd.exe',
+            [
+                '/c',
+                runJbdsFile
+            ],
+            {
+                timeout: 2000,
+            });
+    runJbdsBat.stdout.on('data', 
+                (data) => {
+                    Logger.info("JBDS Start - [" + runJbdsFile + "]: " + data);
+                });
+    runJbdsBat.stderr.on('data', 
+                (data) => {
+                    Logger.info("JBDS Start ERROR - [" + runJbdsFile + "]: " + data);
+                });
+    runJbdsBat.on('exit', 
+                (code) => {
+                    Logger.info("JBDS Start Exit - Code: " + code);
+                    this.exit();
+                });
+  }
+
+  exit() {
+    Logger.info('Closing the installer window');
+    remote.getCurrentWindow().close();
   }
 }
+
+StartController.$inject = ['installerDataSvc'];
 
 export default StartController;
