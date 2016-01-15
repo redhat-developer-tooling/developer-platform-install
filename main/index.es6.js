83eaa23d7ec938a9aa1e22c91249534acf0f3b51
Denis Golovin <dgolovin@gmail.com>
2016-01-15 13:23:55 -0800

    Allow to enable DevTools in installer window
    Use
    
    ``` shell
    export PDKI_DEBUG=
    ```
    
    to enable DevTools or
    
    ``` shell
    unset PDKI_DEBUG
    
    ```
    to disable it.

--- main/index.es6.js
+++ main/index.es6.js
@@ -76,7 +76,9 @@ app.on('ready', function() {
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/../browser/index.html');
 
-  //mainWindow.openDevTools();
+  if( process.env['PDKI_DEBUG'] !== undefined ) {
+    mainWindow.openDevTools();
+  }
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
