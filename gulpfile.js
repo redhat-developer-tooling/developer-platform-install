0379f449dc7ba6543339879b3ab1f47ccaea06ea
nickboldt <nickboldt@gmail.com>
2016-03-23 10:50:50 -0400

    improve performance of 7z extraction & cleanup temporary files when done; also fix typos and remove timestamp/mirror from sourceforge URLs so nearest mirror can be selected
    move into separare task since on long running build (1.6G) the delete can happen BEFORE the copy into the .exe is done

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var gulp = require('gulp'),
     pjson = require('./package.json'),
     reqs = require('./requirements.json'),
     path = require('path'),
+    minimatch = require('minimatch'),
     copy = require('gulp-copy'),
     concat = require('gulp-concat');
 
@@ -75,9 +76,9 @@ gulp.task('run', ['transpile:app'], function(cb) {
   exec(path.join('node_modules', '.bin') + path.sep + 'electron .',createExecCallback(cb));
 });
 
-// Wrap electrom generated app to self extractring 7zip archive
+// Wrap electron-generated app to self extractring 7zip archive
 gulp.task('package', function (cb) {
-  let zaRoot = path.resolve(buildFolderRoot)
+  let zaRoot = path.resolve(buildFolderRoot);
   let zaElectronPackage = path.join(zaRoot, 'DeveloperPlatformInstaller-win32-x64');
   let zaZip = path.join(zaRoot, '7za920.zip');
   let zaExe = path.join(zaRoot, '7za.exe');
@@ -85,16 +86,16 @@ gulp.task('package', function (cb) {
   let zaExtra7z = path.join(zaRoot, '7z920_extra.7z');
   let configTxt = path.resolve(path.join(zaRoot, '..', '..', 'config.txt'));
   let bundled7z = path.join(zaRoot, 'DeveloperPlatformInstaller-w32-x64.7z');
-  let instllerExe = path.join(zaRoot, 'DeveloperPlatformInstaller-win32-x64.exe');
-  request('http://downloads.sourceforge.net/project/sevenzip/7-Zip/9.20/7za920.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fsevenzip%2Ffiles%2F7-Zip%2F9.20%2F&ts=1458670605&use_mirror=pilotfiber')
+  let installerExe = path.join(zaRoot, 'DeveloperPlatformInstaller-win32-x64.exe');
+  request('http://downloads.sourceforge.net/project/sevenzip/7-Zip/9.20/7za920.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fsevenzip%2Ffiles%2F7-Zip%2F9.20%2F')
       .pipe(fs.createWriteStream(zaZip)).on('finish', function () {
     console.log(zaZip);
     gulp.src(path.join(buildFolderRoot, '7za920.zip'))
-        .pipe(unzip())
+        .pipe(unzip({ filter : function(entry){ return minimatch(entry.path, "**/7za.exe") } }))
         .pipe(gulp.dest(buildFolderRoot));
-    request('http://downloads.sourceforge.net/project/sevenzip/7-Zip/9.20/7z920_extra.7z?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fsevenzip%2Ffiles%2F7-Zip%2F9.20%2F&ts=1458670735&use_mirror=iweb')
+    request('http://downloads.sourceforge.net/project/sevenzip/7-Zip/9.20/7z920_extra.7z?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fsevenzip%2Ffiles%2F7-Zip%2F9.20%2F')
         .pipe(fs.createWriteStream(zaExtra7z)).on('finish', function () {
-      var cmd = zaExe + ' e ' + zaExtra7z + ' -o' + zaRoot + ' -y';
+      var cmd = zaExe + ' e ' + zaExtra7z + ' -o' + zaRoot + ' -y ' + '7zS.sfx';
       console.log(cmd);
       exec(cmd, function (err, stdout, stderr) {
         console.log(stdout);
@@ -106,7 +107,7 @@ gulp.task('package', function (cb) {
             console.log(stdout);
             console.log(stderr);
             if (!err) {
-              var packageCmd = 'copy /b ' + zaSfx + ' + ' + configTxt + ' + ' + bundled7z + ' ' + instllerExe;
+              var packageCmd = 'copy /b ' + zaSfx + ' + ' + configTxt + ' + ' + bundled7z + ' ' + installerExe;
               console.log(packageCmd);
               exec(packageCmd, createExecCallback(cb));
             } else {
@@ -124,6 +125,10 @@ gulp.task('package', function (cb) {
 // Create bundled installer
 gulp.task('package-bundle', function() {
   return runSequence('prefetch', 'package');
+});
+
+gulp.task('7zip-cleanup', function() {
+    del([buildFolderRoot + 'DeveloperPlatformInstaller-w32-x64.7z',buildFolderRoot + '7zS.sfx', buildFolderRoot + '7za.exe', buildFolderRoot + '7za920.zip', buildFolderRoot + '7z920_extra.7z'], { force: true });
 });
 
 gulp.task('test', function() {
