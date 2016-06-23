e6a3cf2199d11b72d74dc22e5c87dfd744fa655b
Jan Richter <jrichter@redhat.com>
2016-06-23 12:17:46 +0200

    Add start page ui test

---
+++ test/ui/start-test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let webdriver = browser.driver;
+let context = { pageName: 'Get Started' };
+let breadcrumbBase = require('./breadcrumbs-base');
+
+describe('Getting Started Page', function() {
+  let devstudioButton, closeButton, quickstartLink;
+
+  beforeAll(function() {
+    browser.setLocation('start')
+    .then(function() {
+      devstudioButton = element(By.id('start-submit'));
+      closeButton = element(By.id('exit-btn'));
+      quickstartLink = element(By.id('quickstart-link'));
+    });
+  });
+
+  it('should display a button to start devstudio', function() {
+    expect(devstudioButton.isDisplayed()).toBe(true);
+    expect(devstudioButton.isEnabled()).toBe(true);
+  });
+
+  it('should display a button to close the installer', function() {
+    expect(closeButton.isDisplayed()).toBe(true);
+    expect(closeButton.isEnabled()).toBe(true);
+  });
+
+  it('should display a link to a quickstart on RHD site', function() {
+    expect(quickstartLink.isDisplayed()).toBe(true);
+    expect(quickstartLink.isEnabled()).toBe(true);
+  });
+
+  breadcrumbBase.describeBreadcrumbs(context);
+});
