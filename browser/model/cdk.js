e162c64bbf6689e9922ddecd1fe5558f1f132641
Denis Golovin <dgolovin@gmail.com>
2016-08-10 15:07:33 -0700

    Update CDK to Aug 10th nightly build

--- browser/model/cdk.js
+++ browser/model/cdk.js
@@ -215,6 +215,8 @@ class CDKInstall extends InstallableItem {
         });
       },(result)=>{
         return installer.exec('vagrant box add --name cdkv2 ' + this.boxName , opts, result);
+      },(result)=>{
+        return installer.exec('vagrant plugin install landrush');
       });
       return Util.runPromiseSequence(execs);
     }
