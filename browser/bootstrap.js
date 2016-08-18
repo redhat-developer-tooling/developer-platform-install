e3ab2356e5f07e4b5e8bd14a9faa137dfa163b5a
Denis Golovin <dgolovin@gmail.com>
2016-08-18 14:55:24 -0700

    JBDS-3834 Install fail - what next?

--- browser/bootstrap.js
+++ browser/bootstrap.js
@@ -3,3 +3,32 @@ import mainModule from './main';
 angular.element(document).ready(function() {
   angular.bootstrap(document, [mainModule.name], { strictDi: true });
 });
+
+// Importing this adds a right-click menu with 'Inspect Element' option
+const { remote } = require('electron');
+const { Menu, MenuItem } = remote;
+
+let rightClickPosition = null;
+
+const menu = new Menu();
+const toggleDevToolsItem = new MenuItem({
+  label: 'Toggle Development Tools',
+  accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+  click: () => {
+    remote.getCurrentWindow().toggleDevTools();
+  }
+});
+const inspectElementItem = new MenuItem({
+  label: 'Inspect Element',
+  click: () => {
+    remote.getCurrentWindow().inspectElement(rightClickPosition.x, rightClickPosition.y);
+  }
+});
+menu.append(toggleDevToolsItem);
+menu.append(inspectElementItem);
+
+window.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+  rightClickPosition = {x: e.x, y: e.y};
+  menu.popup(remote.getCurrentWindow());
+}, false);
