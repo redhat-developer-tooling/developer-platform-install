b906ea15c261274420450a35012aa342c6b1c323
Denis Golovin <dgolovin@gmail.com>
2016-08-20 10:30:11 -0700

    JBDS-4005 Use https redirects for cygwin-installer and oc origin cli

--- test/check-requirements.js
+++ test/check-requirements.js
@@ -17,7 +17,7 @@ function checkRequirements() {
   //to check if the url looks like it points to what it is supposed to
   fileNames['cdk.zip'] = 'cdk';
   fileNames['rhel-vagrant-virtualbox.box'] = 'rhel-cdk';
-  fileNames['oc.zip'] = 'origin-client-tools';
+  fileNames['oc.zip'] = 'oc-origin-cli';
   fileNames['cygwin.exe'] = 'cygwin';
   fileNames['jbds.jar'] = 'devstudio';
   fileNames['jdk.msi'] = 'openjdk';
