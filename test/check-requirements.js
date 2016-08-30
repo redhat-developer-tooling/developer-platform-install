ffcb89b94acf7cc55bc46e935a48f0f467c1fe65
Denis Golovin <dgolovin@gmail.com>
2016-08-30 01:51:58 -0700

    Update CDK to latest nightly build from 2016-08-30

--- test/check-requirements.js
+++ test/check-requirements.js
@@ -16,7 +16,7 @@ function checkRequirements() {
 
   //to check if the url looks like it points to what it is supposed to
   fileNames['cdk.zip'] = 'cdk';
-  fileNames['rhel-vagrant-virtualbox.box'] = 'rhel-cdk';
+  fileNames['rhel-vagrant-virtualbox.box'] = 'cdk-vagrant';
   fileNames['oc.zip'] = 'oc-origin-cli';
   fileNames['cygwin.exe'] = 'cygwin';
   fileNames['jbds.jar'] = 'devstudio';
