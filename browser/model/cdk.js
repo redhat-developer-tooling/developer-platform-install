cf7f9e517287716c44a2453758ec69e07647b258
dgolovin <dgolovin@gmail.com>
2016-10-07 14:34:14 -0700

    JBDS-4072 Log only relevant environment variables not all of them
    Fix changes the logging to Path environment variable only to avoid
    saving env variables with passwords in log file.

--- browser/model/cdk.js
+++ browser/model/cdk.js
@@ -151,6 +151,7 @@ class CDKInstall extends InstallableItem {
       + path.delimiter + path.join(vgrPath,'bin')
       + path.delimiter + path.join(cygwinPath,'bin')
       + path.delimiter + vboxPath;
+    Logger.info(this.key + ' - Set PATH environment variable to \'' + env['Path'] + '\'');
     return env;
   }
 
