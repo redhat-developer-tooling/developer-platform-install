e004bbfaaa320577a43ade68085cfdb91b6eaddb
Denis Golovin <dgolovin@gmail.com>
2016-10-24 19:33:48 -0700

    JBDS-3976 Build for Mac OS X

---
+++ gulp-tasks/config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const path = require('path');
+
+let config = {
+  artifactName : 'devsuite',
+  artifactPlatform : process.platform,
+  artifactArch  : process.arch,
+  buildFolderRoot  : '',
+  buildFileNamePrefix : '',
+  // use folder outside buildFolder so that a 'clean' task won't wipe out the cache
+  prefetchFolder : 'requirements-cache',
+  buildFolderPath : '',
+  configIcon : ''
+}
+
+config.buildFolderRoot = path.join('dist', config.artifactPlatform + '-' + config.artifactArch );
+config.buildFileNamePrefix = config.artifactName;
+config.buildFolderPath = path.resolve(config.buildFolderRoot);
+config.configIcon = path.resolve(path.join('resources', config.artifactName + '.ico'));
+
+module.exports = config;
--- gulp-tasks/dist-darwin.js
+++ gulp-tasks/dist-darwin.js
@@ -1,6 +1,8 @@
 module.exports = function(gulp) {
+
   gulp.task('prepare-tools', function(cb) {
     console.log("Skip for darwin build");
     cb();
   });
+
 };
