0d59bdc6ba83c51f49688e752c546004e0607195
sudhirverma <sudhirvvermaa@gmail.com>
2016-12-30 17:58:32 +0530

    Increase unit tests code coverage for downloader

--- test/unit/model/helpers/downloader-test.js
+++ test/unit/model/helpers/downloader-test.js
@@ -8,6 +8,7 @@ import Downloader from 'browser/model/helpers/downloader';
 import Logger from 'browser/services/logger';
 import { Readable, PassThrough, Writable } from 'stream';
 import Hash from 'browser/model/helpers/hash';
+import fs from 'fs-extra';
 chai.use(sinonChai);
 
 describe('Downloader', function() {
@@ -40,6 +41,7 @@ describe('Downloader', function() {
 
   beforeEach(function() {
     sandbox = sinon.sandbox.create();
+    downloader = new Downloader(fakeProgress, function() {}, function() {});
   });
 
   afterEach(function() {
@@ -47,12 +49,10 @@ describe('Downloader', function() {
   });
 
   it('should set totalDownloads to 1 by default', function() {
-    downloader = new Downloader(fakeProgress, function() {}, function() {});
     expect(downloader.totalDownloads).to.equal(1);
   });
 
   it('responseHandler should set the total download size', function() {
-    downloader = new Downloader(fakeProgress, function() {}, function() {});
     let response = { headers: { 'content-length': 1024 } };
 
     downloader.responseHandler(response);
@@ -61,7 +61,6 @@ describe('Downloader', function() {
   });
 
   it('dataHandler should update the progress once time threshold is reached', function() {
-    downloader = new Downloader(fakeProgress, function() {}, function() {});
     fakeProgress.totalSize = 1024;
 
     let data = { length: 512 };
@@ -75,7 +74,6 @@ describe('Downloader', function() {
   });
 
   it('dataHandler should not update the progress before time threshold is reached', function() {
-    downloader = new Downloader(fakeProgress, function() {}, function() {});
     fakeProgress.totalSize = 1024;
 
     let data = { length: 512 };
@@ -105,8 +103,6 @@ describe('Downloader', function() {
   it('endHandler should end the stream', function() {
     let stream = { end: function() {} };
     let streamSpy = sandbox.spy(stream, 'end');
-
-    downloader = new Downloader(fakeProgress, function() {}, function() {});
     downloader.setWriteStream(stream);
     downloader.endHandler(stream);
 
@@ -114,7 +110,6 @@ describe('Downloader', function() {
   });
 
   it('closeHandler should verify downloaded files checksum', function() {
-    downloader = new Downloader(fakeProgress, function() {}, function() {});
     let stub = sandbox.stub(Hash.prototype, 'SHA256').yields('hash');
 
     downloader.closeHandler('file', 'hash', 'url');
@@ -154,7 +149,6 @@ describe('Downloader', function() {
 
     it('should make a request with given options', function() {
       let requestGetSpy = sandbox.spy(request, 'get');
-      downloader = new Downloader(fakeProgress, function() {}, function() {});
       downloader.setWriteStream(new PassThrough());
       downloader.download(options3);
 
@@ -164,7 +158,6 @@ describe('Downloader', function() {
 
     it('should make a request with given url in options', function() {
       let requestGetSpy = sandbox.spy(request, 'get');
-      downloader = new Downloader(fakeProgress, function() {}, function() {});
       downloader.setWriteStream(new PassThrough());
       downloader.download(options);
 
@@ -175,7 +168,6 @@ describe('Downloader', function() {
 
     it('should make a request with \'User-Agent\' header set', function() {
       let requestGetSpy = sandbox.spy(request, 'get');
-      downloader = new Downloader(fakeProgress, function() {}, function() {});
       downloader.setWriteStream(new PassThrough());
       downloader.download(options);
 
@@ -191,7 +183,6 @@ describe('Downloader', function() {
       sandbox.stub(request, 'get').returns(response);
 
       let stream = new PassThrough();
-      downloader = new Downloader(fakeProgress, function() {}, function() {});
       downloader.setWriteStream(stream);
       let endHandler = sandbox.stub(downloader, 'endHandler');
       downloader.download(options);
@@ -207,7 +198,6 @@ describe('Downloader', function() {
       let error = new Error('something bad happened');
 
       let stream = new Writable();
-      downloader = new Downloader(fakeProgress, function() {}, function() {});
       downloader.setWriteStream(stream);
       let errorHandler = sandbox.stub(downloader, 'errorHandler');
       downloader.download(options);
@@ -222,8 +212,6 @@ describe('Downloader', function() {
       sandbox.stub(request, 'get').returns(response);
 
       let stream = new Writable();
-
-      downloader = new Downloader(fakeProgress, function() {}, function() {});
       downloader.setWriteStream(stream);
       sandbox.spy(downloader, 'success');
 
@@ -271,4 +259,59 @@ describe('Downloader', function() {
       expect(successHandler).to.be.calledOnce;
     });
   });
+
+  describe('restartDownload', function() {
+    let options = 'http://example.com/jdk.zip';
+
+    it('should change downloader status from \'Download Failed\' to \'Downloading\'', function() {
+      let spy = sandbox.spy(fakeProgress, 'setStatus');
+      downloader.restartDownload();
+      expect(spy).to.have.been.calledOnce;
+      expect(downloader.downloadSize).to.be.equal(0);
+      expect(downloader.received).to.be.equal(0);
+      expect(downloader.currentSize).to.be.equal(0);
+      expect(fakeProgress.setStatus).to.have.been.calledOnce;
+      expect(fakeProgress.setStatus).to.have.been.calledWith('Downloading');
+    });
+
+    it('should call authDownload for entries that requires authentication', function() {
+      let response = new Readable();
+      sandbox.stub(request, 'get').returns(response);
+      response.auth = function() { return response; };
+      let error = new Error('something bad happened');
+      let stream = new Writable();
+      stream.close = function() {};
+      stream.path = 'key';
+      downloader.setWriteStream(stream);
+      downloader.downloadAuth(options, 'username', 'password', 'key', 'sha');
+      response.emit('error', error);
+      stream.close = function() {};
+      sandbox.stub(fs, 'createWriteStream');
+      sandbox.stub(downloader, 'downloadAuth');
+      downloader.restartDownload();
+      expect(downloader.downloadAuth).to.be.calledOnce;
+      expect(downloader.downloadAuth).to.be.calledWith(options, 'username', 'password', 'key', 'sha');
+    });
+
+    it('should call download method for entries that does not require authentication',function() {
+      let response = new Readable();
+      sandbox.stub(request, 'get').returns(response);
+      response.auth = function() { return response; };
+      let error = new Error('something bad happened');
+      let stream = new Writable();
+      stream.close = function() {};
+      stream.path = 'key';
+      downloader.setWriteStream(stream);
+      downloader.download(options);
+      response.emit('error', error);
+      stream.close = function() {};
+      sandbox.stub(fs, 'createWriteStream');
+      sandbox.stub(downloader, 'download');
+      downloader.restartDownload();
+      expect(downloader.download).to.be.calledOnce;
+      expect(downloader.download).to.be.calledWith(options);
+
+    });
+  });
+
 });
