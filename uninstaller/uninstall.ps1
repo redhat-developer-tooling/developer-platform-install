25db2c3d6b40b412c02e506495fee17247cffe1a
sudhirverma <sudhirvvermaa@gmail.com>
2017-05-24 15:02:27 +0530

    JBDS-4400 Uninstaller fails to remove Virtualbox

--- uninstaller/uninstall.ps1
+++ uninstaller/uninstall.ps1
@@ -1,5 +1,5 @@
 $folder = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
-Copy-Item ./uninstall-helper.ps1 $env:TEMP/uninstall-helper.ps1
+Copy-Item $folder/uninstall-helper.ps1 $env:TEMP/uninstall-helper.ps1
 cd ../..
 Start-Process powershell.exe -ArgumentList $env:TEMP\uninstall-helper.ps1, $folder
