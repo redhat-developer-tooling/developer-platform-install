4a4d30eec09c4c0c812c79ecf823096f7e6cd695
Denis Golovin <dgolovin@gmail.com>
2017-09-19 13:08:52 -0700

    Fix issues with components loading on confirmation page

--- browser/pages/confirm/controller.js
+++ browser/pages/confirm/controller.js
@@ -2,7 +2,6 @@
 
 import Logger from '../../services/logger';
 import Platform from '../../services/platform';
-import ComponentLoader from '../../services/componentLoader';
 
 class ConfirmController {
 
@@ -12,8 +11,6 @@ class ConfirmController {
     this.timeout = $timeout;
     this.installerDataSvc = installerDataSvc;
     this.electron = electron;
-    this.loader = new ComponentLoader(installerDataSvc);
-    this.loader.loadComponents();
     this.installedSearchNote = '';
     this.isDisabled = false;
     this.numberOfExistingInstallations = 0;
@@ -24,63 +21,12 @@ class ConfirmController {
     $scope.detectionStyle = false;
     $scope.virtualization = true;
     $scope.downloadComp = this.detectDownloadedComponents();
-
-    const selectAllLabel = 'Select All Components';
-
-    menu.insert(0, new MenuItem({
-      label: selectAllLabel,
-      click: ()=> {
-        this.sc.$apply(this.selectAll.bind(this));
-      }
-    }));
-
-    const deselectAllLabel = 'Deselect All Components';
-
-
-    menu.insert(1, new MenuItem({
-      label: deselectAllLabel,
-      click: ()=> {
-        this.sc.$apply(this.deselectAll.bind(this));
-      }
-    }));
-
-    menu.insert(2, new MenuItem({
-      label: deselectAllLabel,
-      type: 'separator'
-    }));
-
-    $scope.$on('$destroy', ()=>{
-      restoreMenu();
-    });
-
-    $scope.isConfigurationValid = this.isConfigurationValid;
-
-    // $scope.$watch('$viewContentLoaded', this.initPage.bind(this));
-
-    // this.electron.remote.getCurrentWindow().addListener('focus', ()=>this.timeout(this.activatePage.bind(this), true));
-  }
-
-  selectAll() {
-    let checkboxModel = this.sc.checkboxModel;
-    for (let key in checkboxModel) {
-      let node = checkboxModel[key];
-      if (node.isInstallable && node.isNotDetected()) {
-        node.selectedOption = 'install';
-      }
-    }
-  }
-
-  deselectAll() {
-    let checkboxModel = this.sc.checkboxModel;
-    for (let key in checkboxModel) {
-      checkboxModel[key].selectedOption = 'detected';
-    }
   }
 
   detectDownloadedComponents() {
     let downloadedComponents = [];
     for (let value of this.installerDataSvc.allInstallables().values()) {
-      if(value.isDownloaded() && value.selectedOption == 'install') {
+      if(value.selectedOption == 'install') {
         downloadedComponents.push(value);
       }
       this.downloadComp = downloadedComponents;
