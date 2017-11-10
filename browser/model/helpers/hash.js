9dcbfa31e3fcebc11d1cf9c21a1705598b27d105
Denis Golovin <dgolovin@gmail.com>
2017-11-09 23:26:25 -0800

    Refactor hash to return promise

--- browser/model/helpers/hash.js
+++ browser/model/helpers/hash.js
@@ -3,18 +3,24 @@ const fs = require('fs-extra');
 
 class Hash {
   SHA256(filename, done) {
-    var hash = crypto.createHash('sha256');
-    var readStream = fs.createReadStream(filename);
-    readStream.on('readable', function () {
-      var chunk = readStream.read();
-      if(chunk) {
-        hash.update(chunk);
-      }
-    }).on('close', function () {
-      var hashstring = hash.digest('hex');
-      done(hashstring);
+    return new Promise(function (res,rej) {
+      var hash = crypto.createHash('sha256');
+      var readStream = fs.createReadStream(filename);
+      readStream.on('readable', function () {
+        var chunk = readStream.read();
+        if(chunk) {
+          hash.update(chunk);
+        }
+      }).on('close', function () {
+        var hashstring = hash.digest('hex');
+        if (done) done(hashstring);
+        res(hashstring);
+      }).on('error', function(err) {
+        if (done) done(undefined, err);
+        rej(err);
+      });
     });
-  }
+  };
 }
 
 export default Hash;
