a8d7e1155ce3ae643e5772c909194ef11eec61a1
Denis Golovin <dgolovin@gmail.com>
2017-12-06 11:16:50 -0800

    Add "" around key walues when creating uninstaller item in registry

--- uninstaller/create-uninstaller.ps1
+++ uninstaller/create-uninstaller.ps1
@@ -14,12 +14,12 @@ Get-ChildItem $uninstallItem | where-object { ($_.PSChildName -like "Development
 New-Item -Path "$uninstallItem" -Name "DevelopmentSuite$timeStamp"
 New-ItemProperty -Path $devsuiteItem -Name DisplayName -Value "Red Hat Development Suite"
 New-ItemProperty -Path $devsuiteItem -Name DisplayVersion -Value $versionString
-New-ItemProperty -Path $devsuiteItem -Name InstallLocation -Value $targetLocation
-New-ItemProperty -Path $devsuiteItem -Name Comments -Value $targetLocation
+New-ItemProperty -Path $devsuiteItem -Name InstallLocation -Value "$targetLocation"
+New-ItemProperty -Path $devsuiteItem -Name Comments -Value "$targetLocation"
 New-ItemProperty -Path $devsuiteItem -Name NoRepair -PropertyType DWORD -Value 1
 New-ItemProperty -Path $devsuiteItem -Name NoModify -PropertyType DWORD -Value 1
 New-ItemProperty -Path $devsuiteItem -Name InstallDate -Value $installDate
 New-ItemProperty -Path $devsuiteItem -Name Publisher -Value "RedHat, Inc."
-New-ItemProperty -Path $devsuiteItem -Name UninstallString -Value $uninstallString
-New-ItemProperty -Path $devsuiteItem -Name DisplayIcon -Value $uninstallIcon
+New-ItemProperty -Path $devsuiteItem -Name UninstallString -Value "$uninstallString"
+New-ItemProperty -Path $devsuiteItem -Name DisplayIcon -Value "$uninstallIcon"
 [Environment]::Exit(0);
