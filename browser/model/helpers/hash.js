1917da15871de3084f1b78a0ff9a1d23edd89c37
Sudhir verma <sudhirvvermaa@gmail.com>
2017-12-15 00:32:30 +0530

    Replace hash.js with hasha npm module #1082 (#1119)
    * Replaced hash.js with hasha npm module #1082
    * Fixed unittest

--- browser/model/helpers/hash.js
+++ browser/model/helpers/hash.js
@@ -1,25 +1,9 @@
-const crypto = require('crypto');
-const fs = require('fs-extra');
+const hasha = require('hasha');
+
 
 class Hash {
-  SHA256(filename, done = function() {}) {
-    return new Promise(function (res, rej) {
-      var hash = crypto.createHash('sha256');
-      var readStream = fs.createReadStream(filename);
-      readStream.on('readable', function () {
-        var chunk = readStream.read();
-        if(chunk) {
-          hash.update(chunk);
-        }
-      }).on('close', function () {
-        var hashstring = hash.digest('hex');
-        done(hashstring);
-        res(hashstring);
-      }).on('error', function(err) {
-        done(undefined, err);
-        rej(err);
-      });
-    });
+  SHA256(filename) {
+    return hasha.fromFile(filename, {algorithm: 'sha256'});
   }
 }
 
--- test/unit/model/helpers/hash-test.js
+++ test/unit/model/helpers/hash-test.js
@@ -4,76 +4,30 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import { default as sinonChai } from 'sinon-chai';
 import Hash from 'browser/model/helpers/hash';
-import { Readable } from 'stream';
 chai.use(sinonChai);
 
-const fs = require('fs-extra');
-const crypto = require('crypto');
+const hasha = require('hasha');
 
 describe('Hash', function() {
   let hash = new Hash();
 
   describe('SHA256', function() {
-    let fakeHash = {
-      update: function() {},
-      digest: function() {}
-    };
-    let sandbox, createHashStub, readStreamStub;
-    let stream;
+    let sandbox, fromFile;
 
     beforeEach(function() {
-      stream = new Readable();
-      stream._read = function() {};
       sandbox = sinon.sandbox.create();
-      createHashStub = sandbox.stub(crypto, 'createHash').returns(fakeHash);
-      readStreamStub = sandbox.stub(fs, 'createReadStream').returns(stream);
+      fromFile = sandbox.stub(hasha, 'fromFile').resolves('fakeHash');
     });
 
     afterEach(function() {
       sandbox.restore();
     });
 
-    it('should use crypto to create a sha256 hash', function(done) {
-      hash.SHA256('file', () => {
-        expect(createHashStub).to.have.been.calledOnce;
-        expect(createHashStub).to.have.been.calledWith('sha256');
-        done();
+    it('should return promise resolved to sha256 hash', function() {
+      return hash.SHA256('file').then((sha256)=> {
+        expect(sha256).to.be.equal('fakeHash');
+        expect(fromFile).to.have.been.calledWith('file', {algorithm: 'sha256'});
       });
-      stream.emit('close');
-    });
-
-    it('should read the specified file', function(done) {
-      hash.SHA256('file', () => {
-        expect(readStreamStub).to.have.been.called;
-        expect(readStreamStub).to.have.been.calledWith('file');
-        done();
-      });
-      stream.emit('close');
-    });
-
-    it('should create a hex string when the file is done reading', function(done) {
-      let spy = sandbox.spy(fakeHash, 'digest');
-      hash.SHA256('file', () => {
-        expect(spy).to.have.been.called;
-        expect(spy).to.have.been.calledWith('hex');
-        done();
-      });
-      stream.emit('close');
-    });
-
-    it('should return correct hash after reading all chunks from stream', function(done) {
-      sandbox.restore();
-      let digest = crypto.createHash('sha256').update('string1string2').digest('hex');
-      stream = new Readable();
-      sandbox.stub(stream, 'read').onFirstCall().returns('string1').onSecondCall().returns('string2').onThirdCall().returns(null);
-      sandbox.stub(fs, 'createReadStream').returns(stream);
-      hash.SHA256('file', (result) => {
-        expect(result).to.be.equal(digest);
-        done();
-      });
-      stream.emit('readable');
-      stream.emit('readable');
-      stream.emit('close');
     });
   });
 });
