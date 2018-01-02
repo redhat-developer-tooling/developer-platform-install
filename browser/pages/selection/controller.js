60ce77383a05f0df772f23682a0260ed3a92dfbf
Denis Golovin <dgolovin@gmail.com>
2018-01-02 02:59:19 -0800

    Fix #1120 Select All/Clear All should affect selected channel only

--- browser/pages/selection/controller.js
+++ browser/pages/selection/controller.js
@@ -18,7 +18,6 @@ class SelectionController {
     this.isDisabled = false;
     this.numberOfExistingInstallations = 0;
     this.step = 1;
-    this.filter = {};
 
     this.installables = {};
     $scope.checkboxModel = {};
@@ -27,6 +26,7 @@ class SelectionController {
     $scope.virtualization = true;
     this.channelList = require('../../../channels');
     this.channel_tab = 'containerDev';
+    $scope.componentsInChannel = this.componentsInChannel.bind(this);
 
     for (let [key, value] of this.installerDataSvc.allInstallables().entries()) {
       $scope.checkboxModel[key] = value;
@@ -46,29 +46,24 @@ class SelectionController {
     this.electron.remote.getCurrentWindow().addListener('focus', this.activatePage.bind(this));
   }
 
-  componentsInChannel(key) {
-    this.channel_tab = key;
-    this.filter = Object.values(this.sc.checkboxModel).filter(value=> {
+  componentsInChannel(value) {
+    return Object.values(this.sc.checkboxModel).filter(value=> {
       return this.channel_tab === 'all' || value.channel && value.channel[this.channel_tab];
     });
   }
 
   clearAll() {
-    for (let key in this.sc.checkboxModel) {
-      let node = this.sc.checkboxModel[key];
-      if (node.channel.hasOwnProperty(this.channel_tab)) {
-        node.selectedOption = 'detected';
-      }
-    }
+    this.componentsInChannel(this.channel_tab).forEach((node)=>{
+      node.selectedOption = 'detected';
+    });
   }
 
   selectAll() {
-    for (let key in this.sc.checkboxModel) {
-      let node = this.sc.checkboxModel[key];
-      if (node.isInstallable && node.isNotDetected() && node.channel.hasOwnProperty(this.channel_tab)) {
+    this.componentsInChannel(this.channel_tab).forEach((node)=>{
+      if (node.isInstallable && node.isNotDetected()) {
         node.selectedOption = 'install';
       }
-    }
+    });
   }
 
   initPage() {
