2943e29036e285667f23a1a26cf5b7ddfda56e39
Denis Golovin <dgolovin@redhat.com>
2018-01-30 02:42:49 -0800

    Fix component panel directive unit tets code coverage

--- test/unit/directives/component-panel-test.js
+++ test/unit/directives/component-panel-test.js
@@ -8,13 +8,15 @@ import { default as sinonChai } from 'sinon-chai';
 import 'mock-fs';
 import InstallableItem from 'browser/model/installable-item';
 import InstallerDataService from 'browser/services/data';
+import componentPanel from 'browser/directives/componentPanel';
+import ElectronMock from '../../mock/electron';
 require('../../angular-test-helper');
 require('browser/main');
 
 chai.use(sinonChai);
 
 describe('ComponentPanelDirective', function() {
-  var $compile, $rootScope, scope, sandbox;
+  var $compile, $rootScope, scope, sandbox, electron;
   beforeEach(ngModule('devPlatInstaller'));
   beforeEach(inject(function(_$compile_, _$rootScope_, _$templateCache_) {
     sandbox = sinon.sandbox.create();
@@ -23,12 +25,21 @@ describe('ComponentPanelDirective', function() {
     $rootScope = _$rootScope_;
     let templatePath = path.resolve('./browser/directives/componentPanel.html');
     _$templateCache_.put('directives/componentPanel.html', fs.readFileSync(templatePath, 'utf8'));
+    electron = new ElectronMock();
   }));
   afterEach(function() {
     sandbox.restore();
   });
 
   function buildScope() {
+    let ds = buildSvc();
+    let scope = $rootScope.$new();
+    console.log($rootScope.openUrl);
+    scope.item = new InstallableItem('kompose', 'url', 'kompose.exe', 'kompose', ds, false);
+    return scope;
+  }
+
+  function buildSvc() {
     let ds = sinon.stub(new InstallerDataService());
     ds.getRequirementByName.restore();
     ds.tempDir.returns('temporaryFolder');
@@ -37,9 +48,7 @@ describe('ComponentPanelDirective', function() {
     ds.getPassword.returns('password');
     ds.komposeDir.returns(path.join(ds.installDir(), 'kompose'));
     ds.localAppData.restore();
-    let scope = $rootScope.$new();
-    scope.item = new InstallableItem('kompose', 'url', 'kompose.exe', 'kompose', ds, false);
-    return scope;
+    return ds;
   }
 
   it('generates html with comoponent name and download size 1.00 M', function() {
@@ -59,5 +68,23 @@ describe('ComponentPanelDirective', function() {
       '<component-panel item="item"></component-panel>'))(scope);
     scope.$digest();
     expect(compiledDirective.html()).to.contain('0 MB');
+  });
+
+  it('creates openUrl function that calls electron shell to open external browser', function() {
+    let s = {};
+    sandbox.stub(electron.shell, 'openExternal');
+    componentPanel().controller[3](s, electron, buildSvc());
+    expect(s.openUrl).not.equals(undefined);
+    s.openUrl();
+    expect(electron.shell.openExternal).calledOnce;
+  });
+
+  it('creates evaluateCondition function that evaluates passed expression', function() {
+    let s = {};
+    sandbox.mock(electron.shell.openExternal);
+    componentPanel().controller[3](s, ElectronMock, buildSvc());
+    expect(s.evaluateCondition).not.equals(undefined);
+    let result = s.evaluateCondition('true');
+    expect(result).equals(true);
   });
 });
