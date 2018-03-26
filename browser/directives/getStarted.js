7d4986e382ccb48a65c8020156698465547089c7
Mohit Suman <mohit.skn@gmail.com>
2018-03-26 23:42:56 +0530

    Add directives for final page cards

---
+++ browser/directives/getStarted.js
@@ -0,0 +1,41 @@
+'use strict';
+
+function getStarted() {
+  return {
+    controller: ('getStartedController', ['$scope', 'electron', 'installerDataSvc', function($scope, electron, installerDataSvc) {
+      $scope.checkStatus = function(component) {
+        return installerDataSvc.getInstallable(component).isSkipped();
+      };
+
+      $scope.fetchDesc = function(component) {
+        return installerDataSvc.getInstallable(component).productDesc;
+      };
+
+      $scope.fetchName = function(component) {
+        return installerDataSvc.getInstallable(component).productName;
+      };
+
+      $scope.gotoDocs = function(component) {
+        electron.shell.openExternal('https://developers.redhat.com/products/'+ component +'/docs-and-apis/');
+      };
+    
+      $scope.gotoLearn = function(component) {
+        if(component==='devstudio'){
+          electron.shell.openExternal('https://developers.redhat.com/products/devstudio/learn/');
+        } else if(component==='cdk') {
+          electron.shell.openExternal('https://developers.redhat.com/topics/containers/');
+        }
+      };
+
+
+    }]),
+    restrict: 'E',
+    replace: true,
+    scope: {
+      component: '='
+    },
+    templateUrl: 'directives/getStarted.html'
+  };
+}
+
+export default getStarted;
